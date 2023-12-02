fb2b44a3e9b6f6dfb89a2b56a3fdbbff17e0419b
zeus <136583387+zeusssz@users.noreply.github.com>
2023-12-02 12:05:16 +0530

    Update Navbar.jsx

--- Puzzl App/Navbar.jsx
+++ Puzzl App/Navbar.jsx
@@ -15,28 +15,26 @@ const Navbar = () => {
     );
  }
 
- const styles =  {
-
-  container: {
-    flexDirection: 'row',
-      alignItems: 'center',
-        justifyContent: 'space-between',
-          backgroundColor: 'rgba(255, 255, 255, 0.3)',
-            height: 60,
-              paddingHorizontal: 20,
+const styles =  {
+container: {
+flexDirection: 'row',
+alignItems: 'center',
+justifyContent: 'space-between',
+backgroundColor: 'rgba(255, 255, 255, 0.3)',
+height: 60,
+paddingHorizontal: 20,
 }
 icon: {
-    width: 32,
-      height; 32
+width: 32,
+height; 32
         ;
   }
   logo: {
-    position: 'absolute',
-      top; 0,
-        left; 0,
-          width; 32,
-            height; 32,
-              resizeMode; 'contain',
+position: 'absolute',
+top, left; 0,
+width; 32,
+height; 32,
+resizeMode; 'contain',
         ;
   }
 };
