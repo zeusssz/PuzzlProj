ebf3871e5c56f5e76cfca4271d86337405364e3a
zeus <136583387+zeusssz@users.noreply.github.com>
2023-12-04 17:24:29 +0530

    Fixed App.jsx

--- Puzzl App/App.jsx
+++ Puzzl App/App.jsx
@@ -1,63 +1,32 @@
 import React from 'react';
-import {Text, StyleSheet, TouchableOpacity, SafeAreaView} from 'react-native';
+import { View, ImageBackground, StyleSheet } from 'react-native';
 import Drawer from './Drawer';
-import Navbar from './Navbar'
-return  (
-    HomeScreen = () => {
-      return (
-        <View>
-          <ImageBackground source={require('./pattern.gif')} style={style.background1}>
-            <ImageBackground source={require('./Box.png')} style={styles.background2}>
-            </ImageBackground>
-          </ImageBackground>
-        </View>
-       , 
-}
+import Navbar from './Navbar';
 
-      const styles = () =>
-  
-      }
-     background1: {
-        flex: 1,
-;
-      }
-      background2: {
-      position: 'absolute',
-      bottom; 0,
-      left; 0,
-      right; 0,
-      height; 50,
-            ;
-          }
-          Drawer:{
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={require('./pattern.gif')} style={styles.background1}>
+        <ImageBackground source={require('./Box.png')} style={styles.background2} />
+      </ImageBackground>
+    </View>
+  );
+};
 
-           } ;
-           import { Navbar } from './Navbar';
-      );
-class App extends React.Component {
-  
-  
-  render() {
-    return (
-      <View>
-        <Navbar/> 
-      </View>
-    
-    );
-  }
-}
-    
-      class App extends React.Component {
-  
-  
-        render() {
-          return (
-            <View>
-              <Drawer/>
-            </View>
-          
-          );
-        }
-      }
-            );
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background1: {
+    flex: 1,
+  },
+  background2: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 50,
+  },
+});
+
 export default HomeScreen;
