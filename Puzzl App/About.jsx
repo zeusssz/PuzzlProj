4c531de12be805388da912ffb0e174275ac290bc
zeus <136583387+zeusssz@users.noreply.github.com>
2023-12-08 16:59:40 +0530

    Update About.jsx

--- Puzzl App/About.jsx
+++ Puzzl App/About.jsx
@@ -13,24 +13,15 @@ class AppPage extends React.Component {
         <Text style={styles.heading}>Meet our developers</Text>
         <View style={styles.developers}>
           <View style={styles.developer}>
-            <Image
-              source={{ uri: './agastya.jpg' }} {/* Add the image URLs @HKS */}
-              style={styles.image}
-            />
+            <Image source={{ uri: './agastya.jpg' }} {/* Add the image URLs @HKS */} style={styles.image}/>
             <Text style={styles.name}>Agastya - Lead Developer</Text>
           </View>
           <View style={styles.developer}>
-            <Image
-              source={{ uri: './harr_kabir.jpg' }} {/* Add the image URLs @HKS */}
-              style={styles.image}
-            />
+            <Image source={{ uri: './harr_kabir.jpg' }} {/* Add the image URLs @HKS */} style={styles.image}/>
             <Text style={styles.name}>Harr Kabir - App developer</Text>
           </View>
           <View style={styles.developer}>
-            <Image
-              source={{ uri: './anahita.jpg' }} {/* Add the image URLs @HKS */}
-              style={styles.image}
-            />
+            <Image source={{ uri: './anahita.jpg' }} {/* Add the image URLs @HKS */} style={styles.image}/>
             <Text style={styles.name}>Anahita - Art/UI</Text>
           </View>
         </View>
